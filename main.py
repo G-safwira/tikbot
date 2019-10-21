f369fa12647566c0c6317f8a4259f801e359fd7a
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2019-10-22 01:25:33 +0300

    update

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import logging
-import requests
-from telegram.ext import Updater
-
 from constants import BOTTOKEN
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
+import argparse
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-def error(bot, update):
-    logger.warning('Update "%s" caused error "%s"', bot, update.error)
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
 
-def main():
-    updater = Updater(BOTTOKEN, use_context=True)
+    def _html(self, message):
+        """This just generates an HTML document that includes `message`
+        in the body. Override, or re-write this do do more interesting stuff.
+        """
+        content = f"<html><body><h1>{message}</h1></body></html>"
+        return content.encode("utf8")  # NOTE: must return a bytes object!
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(self._html("hi!"))
 
-    # log all errors
-    dp.add_error_handler(error)
+    def do_HEAD(self):
+        self._set_headers()
 
-    updater.start_webhook(listen='0.0.0.0', port=4000)
-    # Start the Bot
-    updater.start_polling()
-    updater.idle()
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write(self._html("POST!"))
 
-if __name__ == '__main__':
-    #init users
-    main()
+if __name__ == "__main__":
+    server_address = ("0.0.0.0", 4000)
+    httpd = HTTPServer(server_address, S)
+    print(f"Starting httpd server on 0.0.0.0:4000")
+    httpd.serve_forever()
+
