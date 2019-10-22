f4cba203177f3600430114d229c875ee4ef2740f
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2019-10-22 03:06:52 +0300

    mesasges

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from constants import BOTTOKEN
 from messages import NEWCOMMENT, NEWTHREAD
 bot = telegram.Bot(BOTTOKEN)
 #chats = ['tietokilta','tietokila','tiklors']
-chats = ['tietokila']
+chats = ['@tietokila']
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
